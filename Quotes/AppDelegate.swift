e0da0c71ff3d05c6a9a8e99653eac29c2b4e5735
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-05-02 16:26:49 -0400

    Initial Commit

---
+++ Quotes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Quotes
+//
+//  Created by Ben Godfrey on 5/2/20.
+//  Copyright © 2020 Ben Godfrey. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
