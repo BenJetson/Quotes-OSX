e630ad2f8f7216a7b13a8afaeab6d330c0dca8b9
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-05-02 18:02:57 -0400

    Show a menu when the icon is clicked.

--- Quotes/AppDelegate.swift
+++ Quotes/AppDelegate.swift
@@ -17,14 +17,28 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to initialize your application
         if let button = statusItem.button {
             button.image = NSImage(named: NSImage.Name("StatusBarButtonImage"))
-            button.action = #selector(printQuote(_:))
         }
+        constructMenu()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    func constructMenu() {
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(title: "Print Quote",
+                                action: #selector(AppDelegate.printQuote(_:)),
+                                keyEquivalent: "P"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit Quotes",
+                                action: #selector(NSApplication.terminate(_:)),
+                                keyEquivalent: "q"))
+        
+        statusItem.menu = menu
+    }
+    
     @objc func printQuote(_ sender: Any?) {
         let quoteText = "Never put of until tomorrow what you can do the day after tomorrow."
         let quoteAuthor = "Mark Twain"
